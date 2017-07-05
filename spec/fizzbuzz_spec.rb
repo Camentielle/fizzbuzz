0122b4ad9d58968b9c1c5d89e38d44e08630bedb
Kai <thiskaiguy@gmail.com>
2017-07-05 23:09:08 +0100

    Test numbers 1 to 3 Only test numbers up to 100

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,7 +2,7 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed a multiple of 3' do
-    (3..300).each do |n|
+    (3..100).each do |n|
       if n % 3 == 0 && n % 5 != 0
         expect(fizzbuzz(n)).to eq 'fizz'
       end
@@ -10,7 +10,7 @@ describe 'fizzbuzz' do
   end
 
   it 'returns "buzz" when passed a multiple of 5' do
-    (3..300).each do |n|
+    (3..100).each do |n|
       if n % 5 == 0 && n % 3 != 0
         expect(fizzbuzz(n)).to eq 'buzz'
       end
@@ -18,7 +18,7 @@ describe 'fizzbuzz' do
   end
 
   it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
-    (3..300).each do |n|
+    (0..100).each do |n|
       if n % 5 == 0 && n % 3 == 0
         expect(fizzbuzz(n)).to eq 'fizzbuzz'
       end
@@ -26,7 +26,7 @@ describe 'fizzbuzz' do
   end
 
   it 'returns the integer passed to it if it is not a multiple of 3 or 5' do
-    (3..300).each do |n|
+    (1..100).each do |n|
       if n % 5 != 0 && n % 3 != 0
         expect(fizzbuzz(n)).to eq n
       end
